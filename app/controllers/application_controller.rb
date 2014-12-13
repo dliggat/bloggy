521fac60a7ab97e9c7a1fb2c6f54b514dbaa2a44
Dave Liggat <dliggat@gmail.com>
2014-12-13 14:18:24 -0800

    Added a root route and basic root view with timestamp

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def root
+    render text: <<-EOS
+      <html>
+        <body>
+          <p>Hello from #{request.host}. It is <strong>#{Time.now}</strong>!</p>
+        </body>
+      </html>
+    EOS
+  end
 end
