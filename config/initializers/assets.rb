f042a4391df28dbabdb8d74e0f46a2ee45846d62
Dave Liggat <dave@liggat.org>
2015-05-21 12:38:44 -0700

    Whitelist other asset types

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,5 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+# Rails.application.config.assets.precompile << /\.(?:svg|png|jpg|gif|eot|woff|ttf)\z/
+Rails.application.config.assets.precompile += %w( *.png *.gif *.jpg *.eot *.woff *.ttf )
